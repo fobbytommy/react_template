fffec97b7f8a71068528673664b33d77478327be
Tommy Oh <tommyhyungjinoh@gmail.com>
2018-09-29 18:10:08 -0700

    installed bootstrap 4 / imported to index.js / tested in App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="container">
+				<header>
+					<h1>React Template for general websites</h1>
+				</header>
+				<p>
+					<button className="btn btn-lg btn-danger">testing bootstrap</button>
+				</p>
+			</div>
+		);
+	}
 }
 
 export default App;
